409eb19c0c45324760837b23778564a72e4fa6a7
tanzidtahmid <76787122+tanzidtahmid@users.noreply.github.com>
2021-02-08 17:24:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -99,14 +99,10 @@ function addMealToDOM(meal) {
 
   single_mealEl.innerHTML = `
   <div class="single-meal">
-  <h1>${meal.strMeal}</h1>
+  
   <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
-  <div class="single-meal-info">
-  ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
-  ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
-  </div>
-  <div class="main">
-  <p>${meal.strInstructions}</p>
+  <h1>${meal.strMeal}</h1>
+  
   <h2>Ingredients</h2>
   <ul>
   ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
